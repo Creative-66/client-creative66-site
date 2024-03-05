bc2ad4afe13839f80eb4a1be1b78c6815d48216f
Geison 'Geison66' Luiz <geison@gamersclub.com.br>
2024-03-05 16:40:52 -0300

    feat: update style

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: 'Creative66',
-  description: 'Creative66 - Place where awesome COunter-Strike projects are made.',
+  title: 'Creative66 - CS Maps, Plugins & Configs',
+  description: 'Creative66 - Place where awesome Counter-Strike projects are made.',
 }
 
 import Hero from '@/components/hero'
 import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
+import ContactForm from '@/components/contact'
 import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
 
@@ -13,10 +13,10 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
+      {/* <Features /> */}
       <Zigzag />
       <Testimonials />
-      <Newsletter />
+      <ContactForm />
     </>
   )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -20,30 +20,30 @@ export default function Hero() {
         </div>
 
         {/* Hero content */}
-        <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
+        <div className="relative pt-32 pb-5 md:pt-40 md:pb-8">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Landing template for startups</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+          <div className="max-w-3xl mx-auto text-center pb-6 md:pb-8">
+            <h1 className="h1 mb-4" data-aos="fade-up">The place for your project</h1>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">We do high quality maps, server plugins/scripts and configs for Counter-Strike.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#contact-us">Contact us</a>
               </div>
-              <div data-aos="fade-up" data-aos-delay="600">
+              {/* <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-              </div>
+              </div> */}
             </div>
           </div>
 
-          <ModalVideo
+          {/* <ModalVideo
             thumb={VideoThumb}
             thumbWidth={1024}
             thumbHeight={576}
             thumbAlt="Modal video thumbnail"
             video="/videos/video.mp4"
             videoWidth={1920}
-            videoHeight={1080} />
+            videoHeight={1080} /> */}
 
         </div>
 
