dbb7c7857b25374690d7e8b55d1e420a40673128
Geison 'Geison66' Luiz <geison@gamersclub.com.br>
2024-03-11 20:00:32 -0300

    feat: fix typo

--- components/showcases.tsx
+++ components/showcases.tsx
@@ -14,7 +14,7 @@ export default function Showcases() {
         <div className="py-12 md:py-20 border-t border-gray-800">
 
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">what we already do</div>
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">what we already done</div>
             <h2 className="h2 mb-4">previous</h2>
             <p className="text-xl text-gray-400">we already worked for big companies like: Made In Brazil (MIBR), Evil Geniuses (EG), Gamers Club, Nivea, Amazon Prime Video, DreamWorks, Itaú Bank, Monster Energy Drink, Intel, Netflix and more...</p>
           </div>
